735d30db2eb99bff819a1c9b4f8126d59082d6bb
r0bert <r0bert.ethack@gmail.com>
2026-02-15 19:58:30 +0100

    fix: SlotType mapping parser syntax

--- src/EVM/Solidity.hs
+++ src/EVM/Solidity.hs
@@ -123,7 +123,10 @@ instance Read SlotType where
         let
           mFirst = parseTypeName mempty lhs
           mTarget = parseTypeName mempty (T.replace ")" "" (last rhs))
-          mRest = traverse (parseTypeName mempty . T.replace "mapping(" "")) (take (length rhs - 1) rhs)
+          mRest =
+            traverse
+              (parseTypeName mempty . T.replace "mapping(" "")
+              ) (take (length rhs - 1) rhs)
         in case (mFirst, mRest, mTarget) of
              (Just first, Just rest, Just target) ->
                [(StorageMapping (first NonEmpty.:| rest) target, "")]
